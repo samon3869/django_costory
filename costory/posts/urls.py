8606d735a71bde0fb9099261ef393a8c523243b8
KeonHee Lee <power3869@gmail.com>
2021-10-29 13:18:57 +0900

    [Base job] make all Url

---
+++ costory/posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+		# path('', views.index),
+		# path('posts/', views.post_list),
+		# path('posts/<int:post_id>/', views.post_detail),
+		# path('posts/new/', views.post_create),
+		# path('posts/<int:post_id>/edit/', views.post_edit),
+		# path('posts/<int:post_id>/delete/', views.post_delete),
+]
