83340d89a21752b74862a8267a6cdf29987a210b
KeonHee Lee <power3869@gmail.com>
2021-11-07 14:52:24 +0900

    [CREATE: Validator] Form_field Validators

--- costory/posts/forms.py
+++ costory/posts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Post
+from django.core.exceptions import ValidationError
 
 
 class PostForm(forms.ModelForm):
@@ -7,3 +8,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if '*' in title:
+            raise ValidationError('*는 포함될 수 없습니다.')
+
+        return title
