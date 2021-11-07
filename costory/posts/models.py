c6d837cd5cb16b023b4b1f964e17ce5696c2f87a
KeonHee Lee <power3869@gmail.com>
2021-11-07 14:04:14 +0900

    [CREATE: Validator] unique option error_messages

--- costory/posts/models.py
+++ costory/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=50, unique=True)
+    title = models.CharField(max_length=50, unique=True, error_messages={'unique': '이미 있는 제목이네요!'})
     content = models.TextField()
     dt_created = models.DateTimeField(verbose_name="Data Created", auto_now_add=True)
     dt_modified = models.DateTimeField(verbose_name="Data Modified", auto_now=True)
