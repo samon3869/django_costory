8a14628b4bb01c2bead294b89f9bd24afddfc23a
KeonHee Lee <power3869@gmail.com>
2021-11-07 18:00:17 +0900

    [Main url link] url '' -> redirect 'post-list'

--- costory/posts/urls.py
+++ costory/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-		# path('', views.index),
+		path('', views.index),
 		path('posts/', views.post_list, name='post-list'),
 		path('posts/<int:post_id>/', views.post_detail, name='post-detail'),
 		path('posts/new/', views.post_create, name='post-create'),
