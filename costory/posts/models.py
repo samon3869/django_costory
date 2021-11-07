127dda15aad2462ff0da00d8bd3e10278adda550
KeonHee Lee <power3869@gmail.com>
2021-11-07 14:03:04 +0900

    [CREATE: Validator] unique option & after validator logic

--- costory/posts/models.py
+++ costory/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True)
     content = models.TextField()
     dt_created = models.DateTimeField(verbose_name="Data Created", auto_now_add=True)
     dt_modified = models.DateTimeField(verbose_name="Data Modified", auto_now=True)
