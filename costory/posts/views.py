add9a3c0577179d057a520ccd5436323ef0dd8dd
KeonHee Lee <power3869@gmail.com>
2021-11-07 17:41:12 +0900

    [DELETE] post delete

--- costory/posts/views.py
+++ costory/posts/views.py
@@ -37,3 +37,11 @@ def post_update(request, post_id):
     else:
         post_form = PostForm(instance=post)
     return render(request, 'posts/post_form.html', {'form': post_form})
+
+def post_delete(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post-list')
+    else:
+        return render(request, 'posts/post_confirm_delete.html', {'post':post})
