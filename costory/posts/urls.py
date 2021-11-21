5117bb3e4044ea6b6cbbb34ebd1fcc9d7eb1e618
KeonHee Lee <power3869@gmail.com>
2021-11-21 11:01:30 +0900

    [CLASS VIEW] generic view more simply

--- costory/posts/urls.py
+++ costory/posts/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-		path('', views.index),
+		path('', views.IndexRedirectView.as_view(), name='index'),
 		path('posts/', views.PostListView.as_view(), name='post-list'),
-		path('posts/<int:post_id>/', views.PostDetailView.as_view(), name='post-detail'),
+		path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
 		path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-		path('posts/<int:post_id>/edit/', views.PostUpdateView.as_view(), name='post-update'),
-		path('posts/<int:post_id>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+		path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+		path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
--- costory/posts/views.py
+++ costory/posts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.core.paginator import Paginator
 from django.http import Http404
-from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import (
+    CreateView, ListView, DetailView, UpdateView, DeleteView, RedirectView
+)
 from django.urls import reverse
 from .models import Post
 from .forms import PostForm
@@ -10,48 +12,40 @@ from .forms import PostForm
 
 class PostListView(ListView):
     model = Post
-    template_name = 'posts/post_list.html'
-    context_object_name = 'posts'
     ordering = ['-dt_created']
     paginate_by = 6
-    page_kwarg = 'page'
-    
+
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'posts/post_detail.html'
-    pk_url_kwarg = 'post_id'
-    context_object_name = 'post'
     
 
 class PostCreateView(CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'posts/post_form.html'
     
     def get_success_url(self):
-        return reverse('post-detail', kwargs={'post_id': self.object.id})
+        return reverse('post-detail', kwargs={'pk': self.object.id})
         
         
 class PostUpdateView(UpdateView):
     model = Post
     form_class = PostForm
-    template_name = 'posts/post_form.html'
-    pk_url_kwarg = 'post_id'
     
     def get_success_url(self):
-        return reverse('post-detail', kwargs={'post_id': self.object.id})
+        return reverse('post-detail', kwargs={'pk': self.object.id})
 
 
 class PostDeleteView(DeleteView):
     model = Post
-    template_name = 'posts/post_confirm_delete.html'
-    pk_url_kwarg = 'post_id'
-    context_object_name = 'post'
     
     def get_success_url(self):
         return reverse('post-list')
 
 
-def index(request):
-    return redirect('post-list')
+# 이런 간단한 것은 함수형쓰는게 직관적이고 좋음
+# generic view 수업이니까 다루는 것 뿐, 이렇게 하지 않아도 됨
+# def index(request):
+#     return redirect('post-list')
+class IndexRedirectView(RedirectView):
+    pattern_name = 'post-list'
