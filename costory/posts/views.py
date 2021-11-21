2e22262f2c78d875a2b3a18be797cbf314043cb0
KeonHee Lee <power3869@gmail.com>
2021-11-21 10:13:10 +0900

    [CLASS VIEW] generic view into updateview

--- costory/posts/views.py
+++ costory/posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.core.paginator import Paginator
 from django.http import Http404
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from django.urls import reverse
 from .models import Post
 from .forms import PostForm
@@ -33,16 +33,27 @@ class PostCreateView(CreateView):
         return reverse('post-detail', kwargs={'post_id': self.object.id})
         
         
-def post_update(request, post_id):
-    post = Post.objects.get(id=post_id)
-    if request.method == 'POST':
-        post_form = PostForm(request.POST, instance=post)
-        if post_form.is_valid():
-            post_form.save()
-            return redirect('post-detail', post_id=post.id)
-    else:
-        post_form = PostForm(instance=post)
-    return render(request, 'posts/post_form.html', {'form': post_form})
+# def post_update(request, post_id):
+#     post = Post.objects.get(id=post_id)
+#     if request.method == 'POST':
+#         post_form = PostForm(request.POST, instance=post)
+#         if post_form.is_valid():
+#             post_form.save()
+#             return redirect('post-detail', post_id=post.id)
+#     else:
+#         post_form = PostForm(instance=post)
+#     return render(request, 'posts/post_form.html', {'form': post_form})
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+    pk_url_kwarg = 'post_id'
+    
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'post_id': self.object.id})
+
 
 def post_delete(request, post_id):
     post = Post.objects.get(id=post_id)
